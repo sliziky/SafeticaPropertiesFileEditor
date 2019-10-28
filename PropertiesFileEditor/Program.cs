b76e036e9c49610b0612f97860cde860a1979ae0
Peter Bolfa <peterbolfa4@gmail.com>
2019-10-28 13:40:37 +0100

    Rewritten. Added some basic tests

--- PropertiesFileEditor/Program.cs
+++ PropertiesFileEditor/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 namespace PropertiesFileEditor {
-    class Program {
+    static class Program {
         static void Main( string[] args ) {
             PropertyParser propertyParser = new PropertyParser( args[ 0 ], args[ 1 ], args[ 2 ] );
             propertyParser.Parse();
--- PropertiesFileEditor/PropertyParser.cs
+++ PropertiesFileEditor/PropertyParser.cs
@@ -4,20 +4,19 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+
+
 namespace PropertiesFileEditor {
-    class PropertyParser {
+    public class PropertyParser {
         private string _operation;
         private string _fileName;
         private string _property;
         private string TEMP_FILE = "tmpFile";
         private static string KEYWORD = "[SafeticaProperties]";
-        private static int BUFFER_VALUE = 1024;
-        private string _readLine;
+        private static int BUFFER_SIZE = 1024;
+        private static int FOOTER_SIZE = 1024;
         private BinaryWriter _writer;
         private BinaryReader _reader;
-        private FileStream stream;
-        private byte[] _buffer = new byte[ BUFFER_VALUE ];
-            
 
         public PropertyParser( string operation, string fileName, string property ) {
             _operation = operation;
@@ -25,42 +24,28 @@ namespace PropertiesFileEditor {
             _property = property;
             TEMP_FILE += GetFileExtension(fileName);
             _reader = new BinaryReader( new FileStream( _fileName, FileMode.OpenOrCreate ));
-            stream = new FileStream(TEMP_FILE, FileMode.OpenOrCreate);
-            _writer = new BinaryWriter( stream );
+            _writer = new BinaryWriter(new FileStream(TEMP_FILE, FileMode.OpenOrCreate));
+        }
+
+        private void Finalize()
+        {
+            _reader.Close();
+            _writer.Close();
+            File.Delete(_fileName);
+            File.Move(TEMP_FILE, _fileName);
         }
 
         public void Parse() {
-            //Operation operation = new Operation();
-            //if( _fileName.EndsWith( ".txt" ) ) {
-            //    switch( _operation ) {
-            //        case "add": operation = Operation.ADD; break;
-            //        case "edit": operation = Operation.EDIT; break;
-            //        case "remove": operation = Operation.REMOVE; break;
-            //    }
-            //    ParseFile( operation );
-            //}
-            //byte[] lastBytes = ReadLastBytes(1024);
-            //string result = Convert.ToBase64String(lastBytes);
-            //Console.WriteLine(result);
-            ////doThis();
-            //result += "[SafeticaProperties]\\nproperty1=value1";
-            //byte[] data = Encoding.UTF8.GetBytes(result);
-            //string b64 = Convert.ToBase64String(data);
-            //lastBytes = Convert.FromBase64String(b64);
-            //Open the existing file
-            //Stream the existing file across
             CopyOneFileToAnother();
-            byte[] footer = ReadLastBytes(1024);
-           // _writer.Write(footer);
+            byte[] footer = ReadLastBytes(BUFFER_SIZE);
             string footerString = Encoding.UTF8.GetString(footer);
             
             int beginningIndex = footerString.IndexOf("[SafeticaProperties]");
-            bool footerFound = beginningIndex != -1;
+            bool headerFound = beginningIndex != -1;
 
-            // byte[] newArr = stringIntoBytes(footerString);
-
-            if (!footerFound)
+            if (!headerFound)
             {
+                _writer.Write(footer);
                 if (_operation.Equals("add"))
                 {
                     _writer.Write(Encoding.Default.GetBytes(createFooter()));
@@ -70,95 +55,94 @@ namespace PropertiesFileEditor {
                     Finalize();
                     return;
                 }
-            }
+            }   
             else
             {
-                string beforeFooterString = footerString.Substring(0, beginningIndex);
                 string afterFooterString = footerString.Substring(beginningIndex);
                 string properties = afterFooterString.Substring(KEYWORD.Length);
                 string[] splitProperties = properties.Split("\\n");
-                foreach (string s in splitProperties) {
-                    Console.WriteLine(s);
-                }
 
-                string output = "[SafeticaProperties]";
+                byte[] beforeFooterBytes = Helper.SubArray<byte>(footer, 0, beginningIndex);
+                _writer.Write(beforeFooterBytes);
 
-
-                byte[] beforeFooter = Encoding.Default.GetBytes(beforeFooterString);
-                _writer.Write(beforeFooter);
 
 
                 if (_operation.Equals("remove"))
                 {
-                    PropertyItem old = new PropertyItem(_property, "");
-                    for (int i = 1; i < splitProperties.Length; ++i)
-                    {
-                        PropertyItem item = SplitPropertyByEquals(splitProperties[i]);
-                        if (old == item)
-                        {
-                            continue;
-                        }
-                        output += "\\n" + item.ToString();
-                    }
-                    byte[] afterFooter = Encoding.Default.GetBytes(output);
-                    _writer.Write(afterFooter);
+                    RemoveProperty(splitProperties);
                 }
 
                 if (_operation.Equals("edit"))
                 {
-                    // REMOVE
-                    PropertyItem newItem = new PropertyItem(_property);
-                    for (int i = 1; i < splitProperties.Length; ++i)
-                    {
-                        PropertyItem item = SplitPropertyByEquals(splitProperties[i]);
-                        if (newItem == item)
-                        {
-                            item = newItem; 
-                        }
-                        output += "\\n" + item.ToString();
-                    }
-                    byte[] afterFooter = Encoding.Default.GetBytes(output);
-                    _writer.Write(afterFooter);
+                    EditProperty(splitProperties);
                 }
 
                 if (_operation.Equals("add"))
                 {
-                    // ADD
-                    // have to cast it to byte[] because string contains some unwanted characters - dont know why
                     byte[] afterFooter = Encoding.Default.GetBytes(afterFooterString + "\\n" + _property);
-                    _writer.Write(afterFooter);
+                    if (afterFooter.Length <= FOOTER_SIZE)
+                    {
+                        _writer.Write(afterFooter);
+                    }
+
                 }
-                
             }
             Finalize();
         }
 
-        private byte[] stringIntoBytes(string str) {
-            return new byte[str.Length];
+        private void AddProperty() { 
         }
-        private void Finalize() {
-            _reader.Close();
-            _writer.Close();
-            //File.Delete( _fileName );
-            //File.Move( TEMP_FILE, _fileName );
+        private void RemoveProperty(string[] splitProperties)
+        {
+            string output = "[SafeticaProperties]";
+            PropertyItem old = new PropertyItem(_property, "");
+            for (int i = 1; i < splitProperties.Length; ++i)
+            {
+                PropertyItem item = SplitPropertyByEquals(splitProperties[i]);
+                if (old == item)
+                {
+                    continue;
+                }
+                output += "\\n" + item.ToString();
+            }
+            byte[] afterFooter = Encoding.Default.GetBytes(output);
+            _writer.Write(afterFooter);
+        }
+
+        private void EditProperty(string[] splitProperties)
+        {
+            string output = "[SafeticaProperties]";
+            PropertyItem newItem = new PropertyItem(_property);
+            for (int i = 1; i < splitProperties.Length; ++i)
+            {
+                PropertyItem item = SplitPropertyByEquals(splitProperties[i]);
+                if (newItem == item)
+                {
+                    item = newItem;
+                }
+                output += "\\n" + item.ToString();
+            }
+            byte[] afterFooter = Encoding.Default.GetBytes(output);
+           // Console.WriteLine(afterFooter.Length);
+            _writer.Write(afterFooter);
         }
 
         private void CopyOneFileToAnother() {
             _reader.BaseStream.Position = 0;
-            byte[] buffer = new byte[ 1024 ];
+            byte[] buffer = new byte[BUFFER_SIZE];
             int remainingBytes = (int)new FileInfo( _fileName ).Length; 
-            if (remainingBytes > 1024)
+            if (remainingBytes > BUFFER_SIZE)
             {
-                while (remainingBytes >= 2048)
+                while (remainingBytes >= 2 * BUFFER_SIZE)
                 { // skip junk
-                    _reader.Read(buffer, 0, 1024);
-                    _writer.Write(buffer, 0, 1024);
-                    remainingBytes -= 1024;
+                    _reader.Read(buffer, 0, BUFFER_SIZE);
+                    _writer.Write(buffer, 0, BUFFER_SIZE);
+                    remainingBytes -= BUFFER_SIZE;
                 }
-                _reader.Read(buffer, 0, remainingBytes - 1024); // another junk
-                _writer.Write(buffer, 0, remainingBytes - 1024);
-                _reader.Read(buffer, 0, 1024); // most important -> this we will replace with properties
-                //_writer.Write(buffer, 0, 1024);
+                _reader.Read(buffer, 0, remainingBytes - BUFFER_SIZE);
+                _writer.Write(buffer, 0, remainingBytes - BUFFER_SIZE);
+
+                _reader.Read(buffer, 0, BUFFER_SIZE);
             }
             else {
                 _reader.Read(buffer, 0, remainingBytes);
@@ -170,8 +154,7 @@ namespace PropertiesFileEditor {
             return fileName.Substring(extension);
 
         }
-        private byte[] ReadLastBytes(int numberOfBytes) {
-            _reader.BaseStream.Position = 0;
+        public byte[] ReadLastBytes(int numberOfBytes) {
             int fileLength = (int)new FileInfo( _fileName ).Length;
             byte[] bytes;
             if ( fileLength >= numberOfBytes ) {
@@ -180,119 +163,15 @@ namespace PropertiesFileEditor {
                 _reader.Read( bytes, 0, numberOfBytes );
             }
             else {
+                _reader.BaseStream.Position = 0;
                 bytes = new byte[fileLength];
                 _reader.Read(bytes, 0, fileLength);
             }
             return bytes;
         }
 
-        private void ParseFile( Operation operation ) {
-            byte[] buffer = new byte[ BUFFER_VALUE ];
-            using( var reader = new StreamReader( _fileName ) ) {
-                if( reader.BaseStream.Length > 1024 ) {
-                    var x = reader.BaseStream.Seek( -1024, SeekOrigin.End );
-                }
-
-                //reader.Read(buffer, )
-                string line;
-                while( ( line = reader.ReadLine() ) != null ) {
-                    Console.WriteLine( line );
-                }
-            }
-        }
-
-
-        private void parseLine( Operation operation ) {
-            if( _readLine.Contains( KEYWORD ) ) {
-                ParseLineContainingProperties( operation );
-            }
-            else {
-                _writer.Write( _buffer );
-            }
-        }
-
-        private void ParseLineContainingProperties( Operation operation ) {
-            Tuple<string, string> splitLine = SplitLineBeforeAfterHeader( _readLine );
-            string beforeHeader = splitLine.Item1;
-            string afterHeader = splitLine.Item2;
-            _writer.Write( beforeHeader );
-            _writer.Write( KEYWORD );
-
-            afterHeader = RemoveBeginningNewlineChar( afterHeader );
-            if( String.IsNullOrEmpty( afterHeader ) ) {
-                if( operation == Operation.ADD ) {
-                    _writer.Write( "\\n" + _property );
-                }
-                return;
-            }
-
-            IList<PropertyItem> properties = ParseProperties( afterHeader );
-            foreach( PropertyItem propertyItem in properties ) {
-                if( operation == Operation.REMOVE ) {
-                    CheckForPropertyRemoval( propertyItem );
-                }
-
-                if( operation == Operation.EDIT ) {
-                    CheckForPropertyEdit( propertyItem );
-                }
-
-                if( operation == Operation.ADD ) {
-
-                    _writer.Write( "\\n" + afterHeader );
-
-                    if( _buffer.Length < 1024 ) {
-                        _writer.Write( _property );
-                        return;
-                    }
-                }
-            }
-        }
-
-        public void CheckForPropertyRemoval( PropertyItem propertyItem ) {
-            PropertyItem propertyToBeRemoved = new PropertyItem( _property, "" );
-            if( propertyItem == propertyToBeRemoved ) {
-                return;
-            }
-            _writer.Write( "\\n" + propertyItem.Property );
-        }
-
-        public void CheckForPropertyEdit( PropertyItem propertyItem ) {
-            PropertyItem propertyToBeEditted = SplitPropertyByEquals( _property );
-            _writer.Write( "\\n" );
-            if( propertyItem == propertyToBeEditted ) {
-                _writer.Write( propertyToBeEditted.ToString() );
-                return;
-            }
-            _writer.Write( propertyItem.ToString() );
-        }
-
-
-
-
-
-        public IList<PropertyItem> ParseProperties( string line ) {
-            string[] properties = line.Split( "\\n" );
-            IList<PropertyItem> splitProperties = new List<PropertyItem>();
-            foreach( string property in properties ) {
-                splitProperties.Add( SplitPropertyByEquals( property ) );
-            }
-            return splitProperties;
-
-        }
-
         public string createFooter() {
             return "[SafeticaProperties]\\n" + _property;
-        }
-
-
-        public Tuple<string, string> SplitLineBeforeAfterHeader( string line ) {
-            int index = line.IndexOf( KEYWORD );
-            if( index == -1 ) {
-                return new Tuple<string, string>( line, "" );
-            }
-            string beforeProperties = line.Substring( 0, index );
-            string afterProperties = line.Substring( index + "[SafeticaProperties]".Length );
-            return new Tuple<string, string>( beforeProperties, afterProperties );
         }
 
         public PropertyItem SplitPropertyByEquals( string property ) {
@@ -302,14 +181,11 @@ namespace PropertiesFileEditor {
             return new PropertyItem( key, value );
         }
 
-
-        public string RemoveBeginningNewlineChar( string str ) {
-            if( str.Length >= 2 ) {
-                return str.Substring( 2 );
-            }
-            else {
-                return "";
-            }
+        private Tuple<string, string> SplitProperty(string str) {
+            int beginningIndex = str.IndexOf("[SafeticaProperties]");
+            string beforeFooterString = str.Substring(0, beginningIndex);
+            string afterFooterString = str.Substring(beginningIndex);
+            return new Tuple<string, string>(beforeFooterString, afterFooterString);
         }
     }
 }
