76fea799e8be342d196d452b819f9d7ba33763a3
Peter Bolfa <xbolfa@fi.muni.cz>
2019-10-28 23:56:32 +0100

    Refactored

--- PropertiesFileEditor/PropertyParser.cs
+++ PropertiesFileEditor/PropertyParser.cs
@@ -4,20 +4,24 @@ using System.Text;
 
 namespace PropertiesFileEditor
 {
+    /// When speaking of header - we mean header of the footer
+    /// 
+    /// 
     public class PropertyParser
     {
-        private string _operation;
+        private Operation _operation;
         private string _fileName;
         private string _property;
-        private string TEMP_FILE = "tmpFile";
-        private static string KEYWORD = "[SafeticaProperties]";
-        private static int BUFFER_SIZE = 1024;
-        private static int FOOTER_SIZE = 1024;
         private BinaryWriter _writer;
         private BinaryReader _reader;
 
+        public static string TEMP_FILE = "tmpFile";
+        public static string KEYWORD = "[SafeticaProperties]";
+        public static int BUFFER_SIZE = 1024;
+        public static int MAX_FOOTER_SIZE = 1024;
+
         public PropertyParser( string operation, string fileName, string property ) {
-            _operation = operation;
+            _operation = ParseOperation(operation);
             _fileName = fileName;
             _property = property;
             TEMP_FILE += GetFileExtension( _fileName );
@@ -28,8 +32,9 @@ namespace PropertiesFileEditor
         private void OpenFiles() {
             try {
                 _writer = new BinaryWriter( new FileStream( TEMP_FILE, FileMode.OpenOrCreate ) );
-                bool hasWritePermission = Tools.HasWritePermissionOnFile( TEMP_FILE );
-                if ( !hasWritePermission ) throw new UnauthorizedAccessException();
+                // working only on Win
+                // bool hasWritePermission = Tools.HasWritePermissionOnFile( TEMP_FILE );
+                // if ( !hasWritePermission ) throw new UnauthorizedAccessException();
                 _reader = new BinaryReader( new FileStream( _fileName, FileMode.Open ) );
             }
             catch ( FileNotFoundException e ) {
@@ -50,9 +55,13 @@ namespace PropertiesFileEditor
         /// Close the readers 
         /// </summary>
         private void Finalize() {
+            CloseStreams();
+            SaveTheFile();
+        }
+
+        private void CloseStreams() {
             _reader.Close();
             _writer.Close();
-            SaveTheFile();
         }
 
         /// <summary>
@@ -63,60 +72,65 @@ namespace PropertiesFileEditor
             File.Move( TEMP_FILE, _fileName );
         }
 
+        private Operation ParseOperation(string operation) {
+            return operation switch
+            {
+                "edit" => Operation.EDIT,
+                "add" => Operation.ADD,
+                "remove" => Operation.REMOVE,
+                _ => Operation.ADD,
+            };
+        }
+
+        /// <summary>
+        /// Parses file
+        /// </summary>
+        /// First we recieve the last 1024 B or the whole file in bytes
+        /// Then we split those bytes into 2 parts
+        /// 1. Before header
+        /// 2. After header
+        /// Then we write back 'Before header' part
+        /// And we parse the 'After header' part
         public void ParseFile() {
             CopyOneFileToAnother();
+
+
             byte[] lastBytes = ReadLastBytes( BUFFER_SIZE );
             string footerString = Encoding.UTF8.GetString( lastBytes );
-
-            int beginningIndex = footerString.IndexOf( "[SafeticaProperties]" );
+            int beginningIndex = footerString.IndexOf( KEYWORD, StringComparison.Ordinal );
             bool headerFound = beginningIndex != -1;
-
-            Operation operation = new Operation();
-            switch ( _operation ) {
-                case "add": operation = Operation.ADD; break;
-                case "edit": operation = Operation.EDIT; break;
-                case "remove": operation = Operation.REMOVE; break;
-            }
 
             if ( !headerFound ) {
                 _writer.Write( lastBytes );
-                if ( operation == Operation.ADD ) {
-                    _writer.Write( Encoding.Default.GetBytes( createFooter() ) );
-                }
-                if ( operation == Operation.EDIT || operation == Operation.REMOVE ) {
-                    Finalize();
-                    return;
+                if ( _operation == Operation.ADD ) {
+                    _writer.Write( Encoding.Default.GetBytes( CreateFooter() ) );
                 }
             }
             else {
+                byte[] beforeFooter = lastBytes.SubArray( 0, beginningIndex );
+                _writer.Write( beforeFooter );
+
                 string afterFooter = footerString.Substring( beginningIndex );
                 string properties = afterFooter.Substring( KEYWORD.Length );
                 string[] splitProperties = properties.Split( "\\n" );
-
-                byte[] beforeFooterBytes = Tools.SubArray< byte >( lastBytes, 0, beginningIndex );
-                _writer.Write( beforeFooterBytes );
-
-
-                if ( operation == Operation.REMOVE ) {
+                if ( _operation == Operation.REMOVE ) {
                     RemoveProperty( splitProperties );
                 }
 
-                if ( operation == Operation.EDIT ) {
+                if ( _operation == Operation.EDIT ) {
                     EditProperty( splitProperties );
                 }
 
-                if ( operation == Operation.ADD ) {
+                if ( _operation == Operation.ADD ) {
                     AddProperty( afterFooter );
                 }
             }
             Finalize();
         }
-
-        private void AddProperty( string afterFooter ) {
-            byte[] newFooter = Encoding.Default.GetBytes( afterFooter + "\\n" + _property );
-            if ( newFooter.Length <= FOOTER_SIZE ) {
-                _writer.Write( newFooter );
-            }
+        
+        private void AddProperty( string oldFooter ) {
+            string newFooter = oldFooter + "\\n" + _property;
+            WriteNewFooter( newFooter );
         }
 
         /// <summary>
@@ -129,12 +143,18 @@ namespace PropertiesFileEditor
             for ( int i = 1; i < splitProperties.Length; ++i ) { // 0th item is empty, so skip it
                 PropertyItem item = SplitPropertyByEquals( splitProperties[ i ] );
                 if ( old == item ) {
-                    continue;
+                    continue; // skip the property to be removed
                 }
-                output += "\\n" + item.ToString();
+                output += "\\n" + item;
             }
-            byte[] afterFooter = Encoding.Default.GetBytes( output );
-            _writer.Write( afterFooter );
+            WriteNewFooter( output );
+        }
+
+        private void WriteNewFooter(string footer) {
+            byte[] newFooter = Encoding.Default.GetBytes( footer );
+            if( newFooter.Length <= MAX_FOOTER_SIZE ) {
+                _writer.Write( newFooter );
+            }
         }
 
         /// <summary>
@@ -147,37 +167,34 @@ namespace PropertiesFileEditor
             for ( int i = 1; i < splitProperties.Length; ++i ) {
                 PropertyItem item = SplitPropertyByEquals( splitProperties[ i ] );
                 if ( newItem == item ) {
-                    item = newItem;
+                    item = newItem; // edit the property to be editted
                 }
-                output += "\\n" + item.ToString();
+                output += "\\n" + item;
             }
-            byte[] afterFooter = Encoding.Default.GetBytes( output );
-            _writer.Write( afterFooter );
+            WriteNewFooter( output );
         }
 
         /// <summary>
         /// Function copies the given file except the footer into temporary file.
-        /// <para>If the size of file is less than/equal 1024B then we copy the whole file.</para>
+        /// <para>If the size of file is less than/equal 1024 B then we copy the whole file.</para>
         /// </summary>
+        /// We have to copy the whole file except for the last 1024 B
+        /// 
         private void CopyOneFileToAnother() {
-            _reader.BaseStream.Position = 0;
+            
             byte[] buffer = new byte[ BUFFER_SIZE ];
             int remainingBytes = (int)new FileInfo( _fileName ).Length;
             if ( remainingBytes > BUFFER_SIZE ) {
-                while ( remainingBytes >= 2 * BUFFER_SIZE ) { // skip junk
+                // read by chunks
+                while ( remainingBytes >= 2 * BUFFER_SIZE ) { 
                     _reader.Read( buffer, 0, BUFFER_SIZE );
                     _writer.Write( buffer, 0, BUFFER_SIZE );
                     remainingBytes -= BUFFER_SIZE;
                 }
+                // here reaminingBytes are in range <1024, 2048>
                 _reader.Read( buffer, 0, remainingBytes - BUFFER_SIZE );
                 _writer.Write( buffer, 0, remainingBytes - BUFFER_SIZE );
-
-                _reader.Read( buffer, 0, BUFFER_SIZE );
             }
-            else {
-                _reader.Read( buffer, 0, remainingBytes ); // read whole file
-            }
-
         }
 
         /// <summary>
@@ -197,6 +214,7 @@ namespace PropertiesFileEditor
         /// <param name="numberOfBytes">Number of bytes to read</param>
         /// <returns>Array with read bytes</returns>
         public byte[] ReadLastBytes( int numberOfBytes ) {
+            _reader.BaseStream.Position = 0;
             int fileLength = (int)new FileInfo( _fileName ).Length;
             byte[] bytes;
 
@@ -228,7 +246,7 @@ namespace PropertiesFileEditor
         /// Creates the footer
         /// </summary>
         /// <returns>Footer</returns>
-        public string createFooter() {
+        public string CreateFooter() {
             return "[SafeticaProperties]\\n" + _property;
         }
 
@@ -237,25 +255,11 @@ namespace PropertiesFileEditor
         /// </summary>
         /// <param name="property">Property</param>
         /// <returns>PropertyItem </returns>
-        public PropertyItem SplitPropertyByEquals( string property ) {
+        public static PropertyItem SplitPropertyByEquals( string property ) {
             int index = property.IndexOf( '=' );
             string key = property.Substring( 0, index );
             string value = property.Substring( index + 1 );
             return new PropertyItem( key, value );
-        }
-
-        /// <summary>
-        /// Splits line into two parts:
-        /// 1. Before header
-        /// 2. After header
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns>Tuple of two parts</returns>
-        private Tuple<string, string> SplitProperty( string str ) {
-            int beginningIndex = str.IndexOf( "[SafeticaProperties]" );
-            string beforeHeader = str.Substring( 0, beginningIndex );
-            string afterHeader = str.Substring( beginningIndex );
-            return new Tuple<string, string>( beforeHeader, afterHeader );
         }
     }
 }
--- tests/testScript.sh
+++ tests/testScript.sh
@@ -1,103 +1,112 @@
 #!/bin/bash
 file=testFile.dll
+rm testFile.dll
 cp PropertiesFileEditor.dll testFile.dll
-./PropertiesFileEditor.exe remove testFile.dll property1
+./PropertiesFileEditor remove testFile.dll property1
 echo REMOVE WITHOUT FOOTER
+original_size=$(wc -c < "$file")
 DIFF=$(diff PropertiesFileEditor.dll testFile.dll)
 if [ "$DIFF" == "" ]; then
-	echo Good
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
-./PropertiesFileEditor.exe edit testFile.dll property1=value1
+./PropertiesFileEditor edit testFile.dll property1=value1
 echo EDIT WITHOUT FOOTER
 DIFF=$(diff PropertiesFileEditor.dll testFile.dll)
 if [ "$DIFF" == "" ]; then
-	echo Good
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe add testFile.dll property1=value1
+./PropertiesFileEditor add testFile.dll property1=value1
 echo ADDING property1=value1 WITHOUT FOOTER
 size=$(wc -c < "$file")
-if [ $size -eq 9254 ]; then
-	echo Good
+new_size=$(($original_size + 38))
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe edit testFile.dll property1=v1
+new_size=$(($new_size - 4))
+./PropertiesFileEditor edit testFile.dll property1=v1
 echo EDITTING property1=v1 
 size=$(wc -c < "$file")
-if [ $size -eq 9250 ]; then
-	echo Good
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe remove testFile.dll property1
+new_size=$(($new_size - 14))
+./PropertiesFileEditor remove testFile.dll property1
 echo REMOVING property1
 size=$(wc -c < "$file")
-if [ $size -eq 9236 ]; then
-	echo Good
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe add testFile.dll property1=value1
+
+./PropertiesFileEditor add testFile.dll property1=value1
 echo ADDING property1=value1
 size=$(wc -c < "$file")
-if [ $size -eq 9254 ]; then
-	echo Good
+new_size=$(($new_size + 18))
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe add testFile.dll property2=value2
+
+./PropertiesFileEditor add testFile.dll property2=value2
 echo ADDING property2=value2
 size=$(wc -c < "$file")
-if [ $size -eq 9272 ]; then
-	echo Good
+new_size=$(($new_size + 18))
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe remove testFile.dll property3
+./PropertiesFileEditor remove testFile.dll property3
 echo REMOVING NON-EXISTING PROP
 size=$(wc -c < "$file")
-if [ $size -eq 9272 ]; then
-	echo Good
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe edit testFile.dll property3=value3
+./PropertiesFileEditor edit testFile.dll property3=value3
 echo EDITING NON-EXISTING PROP
 size=$(wc -c < "$file")
-if [ $size -eq 9272 ]; then
-	echo Good
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
 
-./PropertiesFileEditor.exe edit testFile.dll property2=newvalue2
+./PropertiesFileEditor edit testFile.dll property2=newvalue2
 echo EDITING property2=newvalue2
 size=$(wc -c < "$file")
-if [ $size -eq 9275 ]; then
-	echo Good
+new_size=$(($new_size + 3))
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
 
-./PropertiesFileEditor.exe remove testFile.dll property1
+new_size=$(($new_size - 18))
+./PropertiesFileEditor remove testFile.dll property1
 echo REMOVING property1
 size=$(wc -c < "$file")
-if [ $size -eq 9257 ]; then
-	echo Good
+if [ $size -eq $new_size ]; then
+    echo Good
 else
-	echo Wrong
+    echo Wrong
 fi
-
-
